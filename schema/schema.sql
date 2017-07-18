5bddf2dc43bbbb56c9b75547252d9d16ad2a6877
Borek Lupomesky <borek@lupomesky.cz>
2017-07-18 15:41:14 +0200

    Make the player and clan names unique in database

--- schema/schema.sql
+++ schema/schema.sql
@@ -1,14 +1,18 @@
 PRAGMA foreign_keys=on;
 
+DROP TABLE clans;
+DROP TABLE players;
+
 CREATE TABLE clans (
   clans_i INTEGER PRIMARY KEY,
-  name text NOT NULL
+  name text NOT NULL UNIQUE
 );
 
 CREATE TABLE players (
   players_i INTEGER PRIMARY KEY,
-  name TEXT NOT NULL,
+  name TEXT NOT NULL UNIQUE,
   pwd TEXT NOT NULL,
   clans_i INT,
+  clan_admin INT NOT NULL DEFAULT 0,
   FOREIGN KEY (clans_i) REFERENCES clans(clans_i)
 );
